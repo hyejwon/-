ac98b8e6ca6f4c4e159d9f09316a99a75e7a634d
“hyejwon” <hyejwon94@gmail.com>
2024-04-06 15:47:19 +0900

    구현 업로드

--- 구현/대표값.py
+++ 구현/대표값.py
@@ -1,23 +1,20 @@
 def main():
-    global n
-    global a
     aver = round(sum(a)/len(a))
-    diffs = []
-    for i in a:
+    score = 0
+    res = 0
+    min_ = 2147000000
+    for i, x in enumerate(a):
         # 평균 차 계산
-        diff = abs(i-aver)
-        diffs.append(diff)
-
-    min_score = min(diffs)
-    scores = dict(zip(a, diffs))
-
-    for k, v in scores.items():
-        if k == min_score:
-            print(v)
-
-
-
-    return aver, scores
+        diff = abs(x-aver)
+        if diff < min_:
+            min_ = diff
+            score = x
+            res = i+1  # 현재 학생 번호
+        elif diff == min_:
+            if x > score:
+                score = x
+                res = i+1
+    return aver, res
 
 
 if __name__ == "__main__":
